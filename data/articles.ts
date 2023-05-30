ce5e2b838113731a11bb04a3b772de96d5edc38c
Pavel Kocman <kocmanpavel@gmail.com>
2023-05-30 17:02:22 +0200

    cange articles content

--- data/articles.ts
+++ data/articles.ts
@@ -1,26 +1,17 @@
 export const articles = [
   {
     id: 1,
-    title: 'What is Lorem Ipsum',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    userId: 9,
-    tags: ['history', 'american', 'crime'],
-    reactions: 2,
+    title: 'What is Stripe, Inc.',
+    body: 'Stripe, Inc. is an Irish-American financial services and software as a service (SaaS) company dual-headquartered in South San Francisco, California, United States and Dublin, Ireland. The company primarily offers payment-processing software and application programming interfaces for e-commerce websites and mobile applications. Stripe provides application programming interfaces that web developers can use to integrate payment processing into their websites and mobile applications. The company introduced Stripe Connect in 2012, a multiparty payments solution that lets software developers embed payments natively into their products.',
   },
   {
     id: 2,
-    title: 'Where does it come from',
-    body: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-    userId: 13,
-    tags: ['french', 'fiction', 'english'],
-    reactions: 2,
+    title: 'Airtable',
+    body: "Airtable is a cloud collaboration service headquartered in San Francisco. It was founded in 2012 by Howie Liu, Andrew Ofstad, and Emmett Nicholas. Airtable is a spreadsheet-database hybrid, with the features of a database but applied to a spreadsheet. The fields in an Airtable table are similar to cells in a spreadsheet, but have types such as 'checkbox', 'phone number', and 'drop-down list', and can reference file attachments like images.[1][2]. Users can create a database, set up column types, add records, link tables to one another, collaborate, sort records and publish views to external websites.",
   },
   {
     id: 3,
-    title: 'Why do we use it',
-    body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    userId: 32,
-    tags: ['magical', 'history', 'french'],
-    reactions: 5,
+    title: 'The story of Next.js',
+    body: "Next.js is a React framework that enables several extra features, including server-side rendering and generating static websites.[8] React is a JavaScript library that is traditionally used to build web applications rendered in the client's browser with JavaScript.[9] Developers recognize several problems with this strategy however, such as not catering to users who do not have access to JavaScript or have disabled it, potential security issues, significantly extended page loading times, and harm to the site's overall search engine optimization.[9] Frameworks such as Next.js sidestep these problems by allowing some or all of the website to be rendered on the server-side before being sent to the client.[9][10] Next.js is one of the most popular frameworks for React.[11] It is one of several recommended toolchains available when starting a new app, all of which provide a layer of abstraction to aid in common tasks.[12] Next.js requires Node.js and can be initialized using Node Package Manager. Google has contributed to the Next.js project, including 43 pull requests in 2019.[13] As of March 2022, the framework is used by many large websites, including Walmart, Apple, Nike, Netflix, TikTok, Uber, Lyft, and Starbucks.[9] In early 2020, Vercel secured twenty-one million dollars in Series A funding to support improvements to the software. The framework's original author, Guillermo Rauch, is currently the CEO of Vercel, and the project's lead maintainer is Tim Neutkens.",
   },
 ];
